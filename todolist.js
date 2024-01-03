8a78082fa4daf53a78d67fbf0b7c781c81152728
Tuğçe Taş <110787805+Tugce-Tas@users.noreply.github.com>
2024-01-03 22:21:51 +0300

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,13 +1,13 @@
-let ALERT= `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+let ALERT= `<div class="alert alert-warning alert-dismissible fade show " role="alert">
 <strong>EKSİK BİLGİ!!</strong>  Lütfen İlgili Alanı Doldurun...
 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>`
+
 let alertDOM=document.querySelector("#alert")
 
 
 let formDOM = document.querySelector("#form")
 formDOM.addEventListener('submit', formHandler)
-
 
 function formHandler(event) {
     event.preventDefault()
@@ -20,34 +20,13 @@ function formHandler(event) {
     inputDOM.value=""
 }
 
-
 function addItem() {
     let ulDOM = document.querySelector("#list")
     let liDOM = document.createElement("li")
     liDOM.innerHTML = `
     <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate"> 
     <label class="form-check-label" for="flexCheckIndeterminate"> ${to_do.value} </label> `
-    liDOM.classList.add("form-check", "list-group-item","px-5")
+    liDOM.classList.add("form-check", "list-group-item","px-5", "mt-2")
     ulDOM.append(liDOM)
 }
-
-
-
-/* <div class="form-check list-group-item"> */
-        //   <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">
-
-        //   <label class="form-check-label" for="flexCheckIndeterminate"> 3 Litre Su İç </label>
-        // </div>
-
-
-
-
-
-/* <div class="form-check list-group-item">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">
-          <label class="form-check-label" for="flexCheckIndeterminate"> 3 Litre Su İç </label>
-        </div> */
-
-
-
 
